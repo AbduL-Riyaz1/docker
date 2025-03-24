24cc5245fd27fa0205cc096b87d164108a5fed41
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-03-24 14:24:38 +0530

    expense-docker

--- docker.sh
+++ docker.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-growpart /dev/nvme0nl 4
-Ivextend -l +50%FREE /dev/RootVG/rootV01
-Ivextend -l +50%FREE /dev/RootVG/varV01
+
+# Resize partitions
+growpart /dev/nvme0n1 4  # Check your actual disk name using `lsblk`
+
+# Extend logical volumes
+lvextend -l +50%FREE /dev/RootVG/rootV01
+lvextend -l +50%FREE /dev/RootVG/varV01
+
+# Grow filesystem
 xfs_growfs /
 xfs_growfs /var
 
+# Install Docker
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start and enable Docker service
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ec2-user
 
+# Add ec2-user to Docker group
+usermod -aG docker ec2-user
 
