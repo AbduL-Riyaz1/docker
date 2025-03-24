0e0599b2f1f3a1fa7765ab2f7c4e312796843c64
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-03-24 14:19:06 +0530

    expense-docker

--- docker.sh
+++ docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-growpart /dev/nvmeenl 4
-Ivextend -1 +50%FREE /dev/RootVG/rootV01
-Ivextend -1 +50%FREE /dev/RootVG/varV01
+growpart /dev/nvme0nl 4
+Ivextend -l +50%FREE /dev/RootVG/rootV01
+Ivextend -l +50%FREE /dev/RootVG/varV01
 xfs_growfs /
 xfs_growfs /var
 
 dnf -y install dnf-plugins-core
-dnf config-manager--add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+
+
